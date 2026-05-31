25a3e1cc6eac5820b0eb7dc6ea9308cfcbd820b2
Pratyanch dongre <69388801+pratyanch-github@users.noreply.github.com>
2026-05-31 13:24:58 +0530

    Initial commit from spring initializer

---
+++ src/test/java/com/noteswala/mynotes/MynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noteswala.mynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
